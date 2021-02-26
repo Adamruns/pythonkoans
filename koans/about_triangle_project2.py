33b8d1a67a6c3ab81a3ad1341284b3b1694ceb6f
Adam Smith <adam.smith@greermiddlecollege.org>
2021-02-26 09:28:35 -0500

    Finished Triangle Project

--- koans/about_triangle_project2.py
+++ koans/about_triangle_project2.py
@@ -6,9 +6,11 @@ from runner.koan import *
 # You need to finish implementing triangle() in the file 'triangle.py'
 from .triangle import *
 
+
 class AboutTriangleProject2(Koan):
     # The first assignment did not talk about how to handle errors.
     # Let's handle that part now.
+
     def test_illegal_triangles_throw_exceptions(self):
         # All sides should be greater than 0
         with self.assertRaises(TriangleError):
@@ -21,5 +23,3 @@ class AboutTriangleProject2(Koan):
             triangle(1, 1, 3)
         with self.assertRaises(TriangleError):
             triangle(2, 5, 2)
-
-
