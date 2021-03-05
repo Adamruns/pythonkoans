76ddb2a2b8648368c53eacdf847fc30b6582a8ee
Adam Smith <adam.smith@greermiddlecollege.org>
2021-03-05 11:17:31 -0500

    Completed but not the extra credit

--- koans/about_proxy_object_project.py
+++ koans/about_proxy_object_project.py
@@ -18,16 +18,39 @@
 
 from runner.koan import *
 
-class Proxy:
-    def __init__(self, target_object):
-        # WRITE CODE HERE
 
-        #initialize '_obj' attribute last. Trust me on this!
+class Proxy(object):
+    def __init__(self, target_object):
+        self._messages = []
+
+        # initialize '_obj' attribute last. Trust me on this!
         self._obj = target_object
 
-    # WRITE CODE HERE
+    def __getattr__(self, attr_name):
+        self._messages.append(attr_name)
+        return self._obj.__getattribute__(attr_name)
 
-# The proxy object should pass the following Koan:
+    def __setattr__(self, attr_name, value):
+        names = ["_obj", "_messages", "messages", "was_called", "number_of_times_called"]
+        if attr_name in names:
+            return object.__setattr__(self, attr_name, value)
+        else:
+            self._messages.append(attr_name + "")
+            self._obj.__setattr__(attr_name, value)
+
+    def messages(self):
+        return self._messages
+
+    def was_called(self, m):
+        return m in self._messages
+
+    def number_of_times_called(self, m):
+        count = 0
+        for message in self._messages:
+            if message == m:
+                count += 1
+
+        return count
 #
 class AboutProxyObjectProject(Koan):
     def test_proxy_method_returns_wrapped_object(self):
